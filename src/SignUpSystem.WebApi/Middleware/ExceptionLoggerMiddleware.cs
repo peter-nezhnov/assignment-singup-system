ae1c256b1b1d02bc3cee3482da513b11efd5a238
Petr Nezhnov <peter.nezhnov@gmail.com>
2018-08-31 00:14:36 +0200

    Fix logger injection

--- src/SignUpSystem.WebApi/Middleware/ExceptionLoggerMiddleware.cs
+++ src/SignUpSystem.WebApi/Middleware/ExceptionLoggerMiddleware.cs
@@ -9,9 +9,9 @@ namespace SignUpSystem.WebApi.Middleware
     public class ExceptionLoggerMiddleware
     {
         private readonly RequestDelegate _next;
-        private readonly ILogger _logger;
+        private readonly ILogger<ExceptionLoggerMiddleware> _logger;
 
-        public ExceptionLoggerMiddleware(RequestDelegate next, ILogger logger)
+        public ExceptionLoggerMiddleware(RequestDelegate next, ILogger<ExceptionLoggerMiddleware> logger)
         {
             _next = next;
             _logger = logger;
